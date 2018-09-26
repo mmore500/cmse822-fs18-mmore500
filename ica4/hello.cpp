05c11fada8ec736908fe2f8aa807f1bf781eb52e
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-09-26 15:10:02 -0400

    Complete exercises 2.3, 2.4, and 2.5

--- ica4/hello.cpp
+++ ica4/hello.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include "mpi.h"
 #include "stdio.h"
 
@@ -16,7 +17,22 @@ int main(int argc, char **argv)
     int resultlen = 0;
     char *name = (char*)malloc(sizeof(char)*MPI_MAX_PROCESSOR_NAME);
     int res = MPI_Get_processor_name(name, &resultlen);
-    printf("%s\n",name);
+    printf("processor name: %s\n",name);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    cout<<"my rank:"<<rank<<endl;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    cout<<"the size:"<<rank<<endl;
+
+
+    char buffer [50];
+    sprintf(buffer,"%d.log",rank);
+    fprintf(fopen(buffer,"w+"),"writing to my own file!");
+
+    if(rank==0){
+      printf("special message from just the root: the size is %d.",size);
+    }
 
     MPI_Finalize();
 
