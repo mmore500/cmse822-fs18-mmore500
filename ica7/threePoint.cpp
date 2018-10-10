c7c8af3bc523c93ecb54bd138f0b5925dcfd1b1b
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-10-10 11:21:17 -0400

    Implement ica7

--- ica7/threePoint.cpp
+++ ica7/threePoint.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <mpi.h>
 #include <vector>
@@ -34,34 +35,113 @@ int main(int argc, char *argv[])
     int rghtNeigh = myRank+1;
     if (myRank==numRanks-1) rghtNeigh = 0;
 
-    if (myRank%2 == 0){
-        // exchange with left neighbor rank
-        MPI_Sendrecv(&x[1],1,MPI_FLOAT,leftNeigh,0,&x[0],1,MPI_FLOAT,leftNeigh,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-    }
-    else {
-        // exchange with right neighbor rank
-        MPI_Sendrecv(&x[nLocal], 1, MPI_FLOAT, rghtNeigh, 0, &x[nLocal+1], 1, MPI_FLOAT, rghtNeigh, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    // now do non-blocking send/recieve
+    MPI_Request* requests = (MPI_Request*)malloc( 2*sizeof(MPI_Request) );
 
-    // Now do the other exchanges
-    if (myRank%2==1){
-        // exchange with left neighbor rank
-        MPI_Sendrecv(&x[1], 1, MPI_FLOAT, leftNeigh, 1, &x[0], 1, MPI_FLOAT, leftNeigh, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    else{
-        // exchange with right neighbor rank
-        MPI_Sendrecv(&x[nLocal], 1, MPI_FLOAT, rghtNeigh, 1, &x[nLocal+1], 1, MPI_FLOAT, rghtNeigh, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    // send to left neighbor rank
+    // tag with 0
+    MPI_Isend(
+        &x[1], /* buf */
+        1, /* count */
+        MPI_FLOAT, /* datatype */
+        leftNeigh, /* dest */
+        0, /* tag */
+        MPI_COMM_WORLD, /* communicator */
+        &requests[0] /* communication request */
+      );
 
-    // for (int i = 0; i < nLocal + 2; i++)
-    // {
-    //     std::cout << i << " " << x[i] << std::endl;
-    // }
+    // send to right neighbor rank
+    // tag with 1
+    MPI_Isend(
+        &x[nLocal], /* buf */
+        1, /* count */
+        MPI_FLOAT, /* datatype */
+        rghtNeigh, /* dest */
+        1, /* tag */
+        MPI_COMM_WORLD, /* communicator */
+        &requests[0] /* communication request */
+      );
 
-    // perform the averaging
-    for (int i = 0; i<nLocal; i++)
+    // perform the averaging on available values
+    for (int i = 1; i < nLocal-1; ++i)
     {
         y[i] = (x[i]+x[i+1]+x[i+2])/3.;
     }
+
+    // recieve from right neighbor rank
+    // tagged with 0
+    MPI_Irecv(
+        &x[nLocal+1], /* buf */
+        1, /* count */
+        MPI_FLOAT, /* datatype */
+        rghtNeigh, /* source */
+        0, /* tag */
+        MPI_COMM_WORLD, /* communicator */
+        &requests[1] /* request */
+      );
+
+    // recieve from left neighbor rank
+    // tagged with 1
+    MPI_Irecv(
+        &x[0], /* buf */
+        1, /* count */
+        MPI_FLOAT, /* datatype */
+        leftNeigh, /* source */
+        1, /* tag */
+        MPI_COMM_WORLD, /* communicator */
+        &requests[1] /* request */
+      );
+
+    MPI_Waitall(2,requests,MPI_STATUSES_IGNORE);
+
+    // perform the averaging on recieved values
+    y[0] = (x[0]+x[1]+x[2])/3.;
+    y[nLocal] = (x[nLocal-1]+x[nLocal]+x[nLocal+1])/3.;
+
+    if (myRank == 0)
+    {
+      std::cout << "x[i]'s " << std::endl;
+      std::cout << std::setw(10) << std::left << "rank"
+        << std::setw(10) << std::left << "i"
+        << std::setw(10) << std::left << "x[i]" << std::endl;
+    }
+
+    for (int j = 0; j < numRanks; j++)
+    {
+      if (myRank == j) {
+        // sanity check
+        for (int i = 0; i < nLocal + 2; i++)
+        {
+          std::cout << std::setw(10) << std::left << myRank
+            << std::setw(10) << std::left << i
+            << std::setw(10) << std::left << x[i] << std::endl;
+        }
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    if (myRank == 0)
+    {
+      std::cout << std::endl << "y[i]'s " << std::endl;
+      std::cout << std::setw(10) << std::left << "rank"
+        << std::setw(10) << std::left << "i"
+        << std::setw(10) << std::left << "y[i]" << std::endl;
+    }
+
+    for (int j = 0; j < numRanks; j++)
+    {
+      if (myRank == j) {
+      // print results
+        for (int i = 0; i < nLocal; i++)
+        {
+          std::cout << std::setw(10) << std::left << myRank
+            << std::setw(10) << std::left << i
+            << std::setw(10) << std::left << y[i] << std::endl;
+        }
+      }
+      MPI_Barrier(MPI_COMM_WORLD);
+    }
+
     MPI_Finalize();
+
 }
