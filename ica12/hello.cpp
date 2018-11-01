9211598beb3e1e9193e1007cb32fc8509819527b
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-11-01 11:43:15 -0400

    Complete ica12

--- ica12/hello.cpp
+++ ica12/hello.cpp
@@ -4,16 +4,22 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    MPI_Init(&argc, &argv);
-    // Get rank/size
-    int myRank, numRanks;
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+  int provided;
 
-    if (myRank == 0) cout << "Hello, World!" << endl;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
+  // Get rank/size
+  int myRank, numRanks;
 
-    MPI_Finalize();
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
 
-    return 0;
+  #pragma omp parallel
+  {
+  if (myRank == 0) cout << "Hello, World!\n" << endl;
+  }
+
+  MPI_Finalize();
+
+  return 0;
 }
